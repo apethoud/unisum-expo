5f9a2bc72a3bcfba1c625c4079e3e821d44c6bb9
Andrew Pethoud <andrewpethoud@gmail.com>
2025-06-13 18:50:47 -0700

    (GameOptions): wire up restart button

--- app/level-view/GameOptions.tsx
+++ app/level-view/GameOptions.tsx
@@ -19,25 +19,35 @@ export default function GameOptions({
   levelStateHistory: ILevelState[],
   setLevelStateHistory: Dispatch<SetStateAction<ILevelState[]>>,
   }) {
-  const undoPlayerMove = () => {
+  const undoPlayerMoves = (numberOfMoves: number) => {
     console.log("undoPlayerMove")
     // remove the last level state from history
     const tempLevelStateHistory = [...levelStateHistory]
-    tempLevelStateHistory.pop()
-    console.log("tempLevelStateHistory AFTER: ", tempLevelStateHistory)
+    console.log("tempLevelStateHistory.length is: ", tempLevelStateHistory.length)
+    // tempLevelStateHistory.pop()
+    const updatedLevelStateHistory = tempLevelStateHistory.slice(0, tempLevelStateHistory.length - numberOfMoves)
+    console.log("updatedLevelStateHistory.length is: ", updatedLevelStateHistory.length)
     // save the updated history
-    setLevelStateHistory(tempLevelStateHistory)
+    setLevelStateHistory(updatedLevelStateHistory)
     // update the current level grid with the new last level state
-    const newLevelState = tempLevelStateHistory[tempLevelStateHistory.length - 1]
+    const newLevelState = updatedLevelStateHistory[updatedLevelStateHistory.length - 1]
     console.log("newLevelState: ", newLevelState)
     setLevelGrid(newLevelState.levelGrid)
     // update the current math options with the new last level state
     setMathOptions(newLevelState.mathOptions)
   }
+
+  const handleUndoPress = () => {
+    undoPlayerMoves(1)
+  }
+
+  const handleRestartPress = () => {
+    undoPlayerMoves(levelStateHistory.length - 1)
+  }
   
-  const GameButton = ({ text }: { text: string }) => (
+  const GameButton = ({ text, onPress }: { text: string, onPress: () => void }) => (
     <Pressable
-      onPress={() => undoPlayerMove()}
+      onPress={onPress}
       disabled={levelStateHistory.length < 2}
     >
       <View className="m-2 p-2 border rounded border-banana-300 bg-banana-100 shadow-sm shadow-slate-300">
@@ -48,8 +58,8 @@ export default function GameOptions({
 
   return (
     <View className="mt-6 flex-row">
-      <GameButton text="Undo" />
-      <GameButton text="Restart" />
+      <GameButton text="Undo" onPress={() => handleUndoPress()} />
+      <GameButton text="Restart" onPress={() => handleRestartPress()} />
     </View>
   )
 }
